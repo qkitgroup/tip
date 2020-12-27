b57c3b6f5fb1b2244c3d176b8ecc33933c9d5960
rotzinger <hannes.rotzinger@gmail.com>
2020-12-27 21:00:28 +0100

    moved webgui to webview

--- start_webview.sh
+++ start_webview.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # start webview with gunicorn webserver with 4 threads, listen locally on port 8000 (0.0.0.0:8000)
-gunicorn web.webgui:server -w 4 -b :8000
+gunicorn web.webview:server -w 4 -b :8000
